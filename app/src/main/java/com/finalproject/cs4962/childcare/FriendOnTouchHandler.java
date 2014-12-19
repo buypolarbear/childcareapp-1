f3999cc2a267dde128620a0a6d905241062fb9c1
aldobaie <akdPro@akds-MacBook-Pro.local>
2014-12-19 09:27:27 -0700

    Selected Person is found
    The selected Friend is known now.

--- app/src/main/java/com/finalproject/cs4962/childcare/FriendOnTouchHandler.java
+++ app/src/main/java/com/finalproject/cs4962/childcare/FriendOnTouchHandler.java
@@ -1,8 +1,12 @@
 package com.finalproject.cs4962.childcare;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
@@ -11,10 +15,11 @@ import android.widget.Toast;
  */
 public class FriendOnTouchHandler implements View.OnTouchListener {
 
+    private static final String TAG = "FriendsList";
     Toast toast;
-    private int padding = 0;
-    private int initialx = 0;
-    private int currentx = 0;
+//    private int padding = 0;
+//    private int initialx = 0;
+//    private int currentx = 0;
    // private  ViewHolder viewHolder;
 
     MotionEvent moEv;
@@ -22,25 +27,37 @@ public class FriendOnTouchHandler implements View.OnTouchListener {
     public boolean onTouch(View view, MotionEvent moEv) {
         //nothing for now i think
 
+
+        Log.i(TAG, ""+((ListView) view.getParent()).getId());
+
         if(toast != null)
             toast.cancel();
 
-       // view.getParent().bringChildToFront(view);
-        if(moEv.getAction()==MotionEvent.ACTION_MOVE)
-             toast = Toast.makeText(view.getContext(), "Touched Ac. Move: "+ view.toString(),Toast.LENGTH_SHORT);
-        else if(moEv.getAction()==MotionEvent.ACTION_CANCEL) {
-
-            toast = Toast.makeText(view.getContext(), "Touched Ac. Cancel: " + view.toString(), Toast.LENGTH_SHORT);
-        }
-        else if(moEv.getAction()==MotionEvent.ACTION_UP) {
-
-            toast = Toast.makeText(view.getContext(), "Touched Ac. Up Delta X: " + (initialx-(int) moEv.getX()), Toast.LENGTH_SHORT);
-        }
-        else if(moEv.getAction()==MotionEvent.ACTION_DOWN) {
-            initialx = (int) moEv.getX();
-
-            toast = Toast.makeText(view.getContext(), "Touched Ac. Down init X: " + initialx, Toast.LENGTH_SHORT);
-        }
+//        view.setBackgroundColor(Color.RED);
+       // view.get
+//        view.
+       // ((ListView) view.getParent()).getItemAtPosition(view.getBottom());
+//        view.getId()
+//        ((ListView) view.getParent()).getAdapter().getItem(((ListView) view.getParent()).getSelectedItemPosition()).toString()
+//        ((ArrayAdapter<ContactRowData>) ((ListView) view.getParent()).getAdapter()).toString()
+//        ((ListView) view.getParent()).getPositionForView((View) view.getParent());
+        toast = Toast.makeText(view.getContext(), "Touched: " +  ((ListView) view.getParent()).getPositionForView(view), Toast.LENGTH_SHORT);
+//       // view.getParent().bringChildToFront(view);
+//        if(moEv.getAction()==MotionEvent.ACTION_MOVE)
+//             toast = Toast.makeText(view.getContext(), "Touched Ac. Move: "+ view.toString(),Toast.LENGTH_SHORT);
+//        else if(moEv.getAction()==MotionEvent.ACTION_CANCEL) {
+//
+//            toast = Toast.makeText(view.getContext(), "Touched Ac. Cancel: " + view.toString(), Toast.LENGTH_SHORT);
+//        }
+//        else if(moEv.getAction()==MotionEvent.ACTION_UP) {
+//
+//            toast = Toast.makeText(view.getContext(), "Touched Ac. Up Delta X: " + (initialx-(int) moEv.getX()), Toast.LENGTH_SHORT);
+//        }
+//        else if(moEv.getAction()==MotionEvent.ACTION_DOWN) {
+//            initialx = (int) moEv.getX();
+//
+//            toast = Toast.makeText(view.getContext(), "Touched Ac. Down init X: " + initialx, Toast.LENGTH_SHORT);
+//        }
 
         toast.show();
 
@@ -66,5 +83,18 @@ public class FriendOnTouchHandler implements View.OnTouchListener {
 
 //MainActivity.Contact_Data.get(view.get)
         return false;
+    }
+
+
+    public View getViewByPosition(int pos, ListView listView) {
+        final int firstListItemPosition = listView.getFirstVisiblePosition();
+        final int lastListItemPosition = firstListItemPosition + listView.getChildCount() - 1;
+
+        if (pos < firstListItemPosition || pos > lastListItemPosition ) {
+            return listView.getAdapter().getView(pos, null, listView);
+        } else {
+            final int childIndex = pos - firstListItemPosition;
+            return listView.getChildAt(childIndex);
+        }
     }
 }
